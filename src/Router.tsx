6b68c75cb6205b07a4e09212544f76d9836f48b7
bichikim <bichi@live.co.kr>
2024-09-24 15:40:01 +0900

    add calculating practice on the routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import {HomePage} from 'src/pages/home/HomePage'
+import {Calculating} from 'src/pages/practice/Calculating'
 import type {Router as RouterType} from '@remix-run/router'
 
 const router: RouterType = createBrowserRouter([
   {
     Component: HomePage,
     path: '/',
+  },
+  {
+    path: '/practice/calculating',
+    Component: Calculating,
   },
 ])
 
