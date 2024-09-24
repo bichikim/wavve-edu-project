7397e702f9aaac2734ea54a45bd87c328c7a6e1a
bichikim <bichi@live.co.kr>
2024-09-24 15:28:36 +0900

    update home

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,6 @@ import {UseEvent} from 'src/use/event/UseEvent'
 export const HomePage = () => {
   return (
     <main className="text-purple">
-      hello home
       <UseEvent />
     </main>
   )
